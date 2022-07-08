f39717bbd0285ca2b8ab927826444572b877eac9
Cristian Herrera <cherreragiraldo@gmail.com>
2022-07-08 00:23:33 -0400

    updated base image

---
+++ devops/base-image/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -s "$HOME/composer.phar" /usr/local/bin/composer
+ln -s "$NODE_BIN/node" /usr/local/bin/node
+ln -s "$NODE_BIN/npm" /usr/local/bin/npm
+ln -s "$NODE_BIN/corepack" /usr/local/bin/corepack
