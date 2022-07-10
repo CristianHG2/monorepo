10c6b4885acd8e8bbd03ff5977f4c80e408bede3
Cristian Herrera <cherreragiraldo@gmail.com>
2022-07-10 03:23:31 -0400

    add zip ext

--- devops/base-image/supports/yarn-packagist.sh
+++ devops/base-image/supports/yarn-packagist.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-NPM_TOKEN=$1
+NPM_AUTH_TOKEN=$1
 COMPOSER_TOKEN=$2
 
 composer config --auth http-basic.repo.packagist.com CristianHG2 "$COMPOSER_TOKEN"
 yarn config set cacheFolder "$HOME/.cache/yarn/v6"
 yarn config set npmScopes.lifespikes.npmRegistryServer "https://npm.pkg.github.com"
 yarn config set npmScopes.lifespikes.npmAlwaysAuth true
-yarn config set npmScopes.lifespikes.npmAuthToken "$NPM_TOKEN"
+yarn config set npmScopes.lifespikes.npmAuthToken "$NPM_AUTH_TOKEN"
