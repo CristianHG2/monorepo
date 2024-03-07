ff7faebb0ef3f463e3783b6481dc346bfce72503
CristianHG2 <cherreragiraldo@gmail.com>
2024-03-07 15:15:53 -0500

    fix: misc fixes and updates

--- so-discord-bot/src/index.ts
+++ so-discord-bot/src/index.ts
@@ -1,23 +1,17 @@
-import invoice from './apps/invoice';
-
-process.env.TZ = 'America/Bogota';
-
 import 'dotenv/config';
 import setUpLogger from './support/debug/logger';
 import {startDiscordBot} from './services/discord';
 import {prepareJob} from './support/scheduler';
 import state from './support/state';
-import badWords from './apps/bad-words';
-import standup from './apps/standup';
 import devtools from './apps/devtools';
-import breakTimer from './apps/break-timer';
-import ocmi from './apps/ocmi';
+import standup from './apps/standup';
 import {BotApp} from './types/apps';
 import log from 'loglevel';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
-import notion from './apps/notion/assignments';
+
+process.env.TZ = 'America/Bogota';
 
 setUpLogger('debug');
 
@@ -27,7 +21,7 @@ dayjs.extend(localizedFormat);
 const enabledApps = {
   // badWords,
   standup,
-  // devtools,
+  devtools,
   // breakTimer,
   // invoice,
   // ocmi,
